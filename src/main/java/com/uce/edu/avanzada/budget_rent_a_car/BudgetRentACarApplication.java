a1c497e7c4565c388d4711966ad15d6afebbc5aa
Emil2245 <emilmontiel@hotmail.com>
2024-03-02 16:35:30 -0500

    first commit

---
+++ src/main/java/com/uce/edu/avanzada/budget_rent_a_car/BudgetRentACarApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.avanzada.budget_rent_a_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetRentACarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetRentACarApplication.class, args);
+    }
+
+}
