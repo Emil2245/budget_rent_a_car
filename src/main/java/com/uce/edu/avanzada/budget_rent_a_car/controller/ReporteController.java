fc6bc4f8df9afb7779ba388d09fb1771da7b0a67
kevoaac <130163890+kevoaac@users.noreply.github.com>
2024-03-05 22:23:43 -0500

    arreglo vista inicio

--- src/main/java/com/uce/edu/avanzada/budget_rent_a_car/controller/ReporteController.java
+++ src/main/java/com/uce/edu/avanzada/budget_rent_a_car/controller/ReporteController.java
@@ -23,47 +23,47 @@ import java.util.List;
 @Controller
 @RequestMapping("/reportes")
 public class ReporteController {
-	
-	private static final Logger LOG = Logger.getLogger(ReporteController.class);
-	@Autowired
-	private IVehiculoService iVehiculoService;
-	
-	@Autowired
-	private IReservaService iReservaService;
 
-	@GetMapping("/reservas")
-	public String reporteReservas(Reserva Reserva) {
-		return "vReporteReserva";
-	}
+    private static final Logger LOG = Logger.getLogger(ReporteController.class);
+    @Autowired
+    private IVehiculoService iVehiculoService;
 
-	@PostMapping("/listaReservas")
-	public String listaReporteReservas(LocalDate fechaInicio, LocalDate fechaFinal, Model model) {
-		System.out.println("fechas: " + fechaFinal + " " + fechaInicio);
-		List<Reserva> lista = this.iReservaService.reporteReserva(fechaInicio, fechaFinal);
-		model.addAttribute("reservas", lista);
-		LOG.info("Generando reporte con éxito");
-		return "vListaReporteReserva";
-	}
-	
-	
-//	@GetMapping("/reservasdto")
+    @Autowired
+    private IReservaService iReservaService;
+
+    @GetMapping("/reservas")
+    public String reporteReservas(Reserva Reserva) {
+        return "vReporteReserva";
+    }
+
+    @PostMapping("/listaReservas")
+    public String listaReporteReservas(LocalDate fechaInicio, LocalDate fechaFinal, Model model) {
+        System.out.println("fechas: " + fechaFinal + " " + fechaInicio);
+        List<Reserva> lista = this.iReservaService.reporteReserva(fechaInicio, fechaFinal);
+        model.addAttribute("reservas", lista);
+        LOG.info("Generando reporte con éxito");
+        return "vListaReporteReserva";
+    }
+
+
+    //	@GetMapping("/reservasdto")
 //	public String reporteReservasDTO(Reserva Reserva) {
 //		return "vReporteReserva";
 //	}
-	@GetMapping("/verReporteReserva")
-	public String reporteReserva(ReservaClienteTO reservaClienteTO, Model model) {
-	    //List<ReporteDTO> reporte = this.iReservaService.reporteDeReservasDTO(reservaClienteTO.getFechaInicioReserva(), reservaClienteTO.getFechaFinReserva());
-	    
-	    model.addAttribute("reservaClienteTO",new ReservaClienteTO());
-	    
-	    
-	    try {
-	       List<ReporteDTO> reporte = this.iReservaService.reporteDeReservasDTO(reservaClienteTO.getFechaInicioReserva(), reservaClienteTO.getFechaFinReserva());    
-	       model.addAttribute("listaReporte",reporte);
-	    } catch (Exception e) {
-	       model.addAttribute("listaReporte", Arrays.asList(new ReporteDTO()));
-	    }
-	    return "reservas/vistaReporteReserva";
+    @GetMapping("/verReporteReserva")
+    public String reporteReserva(ReservaClienteTO reservaClienteTO, Model model) {
+        //List<ReporteDTO> reporte = this.iReservaService.reporteDeReservasDTO(reservaClienteTO.getFechaInicioReserva(), reservaClienteTO.getFechaFinReserva());
 
-	}
+        model.addAttribute("reservaClienteTO", new ReservaClienteTO());
+
+
+        try {
+            List<ReporteDTO> reporte = this.iReservaService.reporteDeReservasDTO(reservaClienteTO.getFechaInicioReserva(), reservaClienteTO.getFechaFinReserva());
+            model.addAttribute("listaReporte", reporte);
+        } catch (Exception e) {
+            model.addAttribute("listaReporte", Arrays.asList(new ReporteDTO()));
+        }
+        return "reservas/vistaReporteReserva";
+
+    }
 }
